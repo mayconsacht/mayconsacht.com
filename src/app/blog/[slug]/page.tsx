44d5aacbb5f1fe37d4f00ef1423f79b9d0ba844b
mayconsacht <maycon.sacht@gmail.com>
2024-09-14 03:28:19 -0300

    refac: clean methods

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import { GetStaticPaths } from 'next';
 import { getAllPosts, getPostBySlug } from 'lib/api';
 import markdownToHtml from 'lib/markdownToHtml';
 import BlogPost from '../BlogPost';
@@ -20,41 +19,3 @@ const Post = async ({ params }: { params: { slug: string } }) => {
     />
   );
 };
-
-type StaticProps = {
-  params: {
-    slug: string;
-  };
-};
-
-export async function generateStaticParams({ params: { slug } }: StaticProps) {
-  console.log('execute it brow');
-  const post = getPostBySlug(slug);
-  const content = await markdownToHtml(post?.content || '');
-
-  const allPosts = getAllPosts();
-  const currentPostIndex = allPosts.findIndex((p) => p.slug === slug);
-  const nextPost = allPosts[currentPostIndex - 1] ?? null;
-  const prevPost = allPosts[currentPostIndex + 1] ?? null;
-  console.log('working');
-  return {
-    params: {
-      post,
-      content,
-      nextPost,
-      prevPost,
-    },
-  };
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const posts = getAllPosts();
-  const paths = posts.map(({ slug }) => ({ params: { slug } }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export default Post;
