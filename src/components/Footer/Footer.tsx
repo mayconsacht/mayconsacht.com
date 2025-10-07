8f24d9a834783de52f575309f84327de3156fe67
Maycon Sacht <mayconsacht@users.noreply.github.com>
2025-10-07 16:30:56 -0300

    chore: update footer date

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,11 @@
-import { ContactLinks } from './ContactLinks';
-import styles from './Footer.module.sass';
+import { ContactLinks } from './ContactLinks'
+import styles from './Footer.module.sass'
 
 export const Footer = () => {
   return (
     <footer className={styles.container}>
-      <span>Maycon Sacht © 2024</span>
+      <span>Maycon Sacht © 2025</span>
       <ContactLinks />
     </footer>
-  );
-};
+  )
+}
